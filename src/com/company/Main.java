af72e061a3ecc908e9200e103d0d77c030b92280
Matthew <Matthew@DESKTOP-FBBVVQH>
2018-10-02 12:59:43 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your first number?");
+        int num1 = input.nextInt();
+
+        System.out.print("What is your second number?");
+        int num2 = input.nextInt();
+
+
+    }
+}
